a17bfe57578dfb83195fa15e8820cd3dab31e953
快乐浪子哥 <hulangfy@163.com>
2023-02-12 13:22:57 +0800

    update

--- src/ISkeleton.php
+++ src/ISkeleton.php
@@ -11,39 +11,55 @@ interface ISkeleton
 {
     /**
      * 创建表
-     * @param $tableModel TableModel
-     * @param $columnModels
-     * @return mixed
+     * @access public
+     * @param TableModel $tableModel 表模型
+     * @param array $columnModels 字段生成模型
+     * @return mixed|array
      */
-    public function createTable(TableModel $tableModel,  array $columnModels);
+    public function createTable(TableModel $tableModel, array $columnModels);
 
     /**
      * 修改表
-     * @return mixed
+     * @access public
+     * @param TableModel $oldtableModel 需要修改表
+     * @param TableModel $newtableModel 新表模型
+     * @return mixed|array
      */
     public function alterTable(TableModel $oldtableModel, TableModel $newtableModel);
 
     /**
      * 删除表
-     * @return mixed
+     * @access public
+     * @param TableModel $tableModel 表模型
+     * @return mixed|array
      */
     public function dropTable(TableModel $tableModel);
 
     /**
      * 添加字段
-     * @return mixed
+     * @access public
+     * @param TableModel $tableModel 表模型
+     * @param ColumnModel $columnModel 字段生成模型
+     * @return mixed|array
      */
     public function addColumn(TableModel $tableModel, ColumnModel $columnModel);
 
     /**
      * 修改字段
-     * @return mixed
+     * @access public
+     * @param TableModel $tableModel 表模型
+     * @param ColumnModel $oldcolumnModel 修改字段模型
+     * @param ColumnModel $newcolumnModel 新字段模型
+     * @return mixed|array
      */
     public function changeColumn(TableModel $tableModel, ColumnModel $oldcolumnModel, ColumnModel $newcolumnModel);
 
     /**
      * 删除字段
-     * @return mixed
+     * @access public
+     * @param TableModel $tableModel 表模型
+     * @param ColumnModel $columnModel 字段生成模型
+     * @return mixed|array
      */
     public function dropColumn(TableModel $tableModel, ColumnModel $columnModel);
 }
